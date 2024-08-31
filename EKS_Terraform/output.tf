923300c0066525c33a907018069f71e6c66a0800
waghmarepritam <117297176+waghmarepritam@users.noreply.github.com>
2024-08-31 17:28:17 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.project.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.project.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.project_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.project_subnet[*].id
 }
